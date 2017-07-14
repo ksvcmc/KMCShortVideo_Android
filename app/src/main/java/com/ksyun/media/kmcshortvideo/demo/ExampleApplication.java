6253867cc68891dc9bb79db5deb66ad4549595ca
sujia <sujia@kingsoft.com>
2017-07-14 17:23:39 +0800

    don't use bugly for now, fix the issue that filter and rotate or scale not take effect at the same time

--- app/src/main/java/com/ksyun/media/kmcshortvideo/demo/ExampleApplication.java
+++ app/src/main/java/com/ksyun/media/kmcshortvideo/demo/ExampleApplication.java
@@ -2,8 +2,6 @@ package com.ksyun.media.kmcshortvideo.demo;
 
 import android.app.Application;
 
-import com.tencent.bugly.crashreport.CrashReport;
-
 /**
  * Created by sujia on 2017/7/14.
  */
@@ -17,6 +15,6 @@ public class ExampleApplication extends Application {
          * 建议在测试阶段建议设置成true，发布时设置为false。
          * Bugly为应用崩溃日志收集工具，开发者可根据实际情况选择不集成或依赖其它Bug收集工具
          */
-        CrashReport.initCrashReport(getApplicationContext(), "4e98881bde", false);
+//        CrashReport.initCrashReport(getApplicationContext(), "4e98881bde", false);
     }
 }
